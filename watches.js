36e5155959d25fa11ff3370ca02497cc50b53c6e
Elias <liasides.elias@gmail.com>
2026-06-06 19:13:14 +0300

    Update Nautilus image to patek.png

--- watches.js
+++ watches.js
@@ -33,7 +33,7 @@ let watches = [
     set: "Box + Papers",
     caseSize: "40mm",
     status: "In Stock",
-    image: "assets/gen_patek_1_1776373728459.png",
+    image: "assets/patek.png",
   },
   {
     brand: "Audemars Piguet",
